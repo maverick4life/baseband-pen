2522065279fc5d43750ffdb3b154afd6d954b12d
Debian <ec2-user@localhost.localdomain>
2017-04-29 09:58:14 +0000

    added screen auto load

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ sudo aot-get upgrade
 cp .screenrc ~/
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-git clone https://github.com/leebaird/discover /opt/discover/
+git clone https://github.com/leebaird/discover ~/discover/
+sudo cp -R ~/discover/ /opt/
 cd /opt/discover/
-./update.sh
+sudo ./update.sh
 cd
 touch ~/discover.installed-at-opt-discover
 
