ddc6b05fd4b0c9be7846b320b63a3bfd22bf03ad
MK <mk@maverickinfosec.com>
2017-04-29 02:34:39 -0700

    added some shit

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
-#!/bin/bash        
+#!/bin/bash
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo aot-get upgrade
 cp .screenrc ~/
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+git clone https://github.com/leebaird/discover /opt/discover/
+cd /opt/discover/
+./update.sh
+cd
+touch ~/discover.installed-at-opt-discover
+
