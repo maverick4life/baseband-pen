178a3e404fdc5ce4bafe5d562ef2b997d84de0aa
maverick4life <mk@maverickinfosec.com>
2017-05-07 01:34:54 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get dist-upgrade
-sudo aot-get upgrade
+sudo apt-get upgrade
 cp .screenrc ~/
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 git clone https://github.com/leebaird/discover ~/discover/
 sudo cp -R ~/discover/ /opt/
-cd /opt/discover/
-sudo ./update.sh
-cd
+sudo /opt/discover/update.sh
 touch ~/discover.installed-at-opt-discover
-
