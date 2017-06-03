95f5f02a3544dd0effd37ad198759a255ff768ce
maverick4life <mk@maverickinfosec.com>
2017-06-03 09:59:18 -0700

    enhance

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 sudo apt-get -y install screen git
-#sudo apt-get -y install git
 sudo apt-get -y update
 sudo apt-get -y upgrade
 cp "$parent_path/.screenrc" ~/
+mkdir ~/pentest-guides/
+wget -o ~/pentest-guides/index.html https://highon.coffee/blog/penetration-testing-tools-cheat-sheet/
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 git clone https://github.com/leebaird/discover ~/discover/
