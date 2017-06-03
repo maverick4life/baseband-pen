6ca9e2b93aab4181074f3774d19fb72810d37eca
MK <mk@maverickinfosec.com>
2017-06-03 08:54:41 -0700

    updated install.sh to use relative paths and apt-get -y

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install screen
-sudo apt-get install git
-cp .screenrc ~/
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+sudo apt-get -y install screen
+sudo apt-get -y install git
+sudo apt-get -y update
+sudo apt-get -y upgrade
+cp "$parent_path/.screenrc" ~/
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 git clone https://github.com/leebaird/discover ~/discover/
